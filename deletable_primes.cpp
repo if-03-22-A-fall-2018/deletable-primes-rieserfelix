22a2349b917fd900bb0225085973664f594d45bc
Felix <dafelix.rieser@gmail.com>
2019-02-25 19:24:45 +0100

    finished

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -1,12 +1,56 @@
-/*----------------------------------------------------------
- *				HTBLA-Leonding / Class
- * ---------------------------------------------------------
- * Exercise Number: 0
- * Title:			deletable_primes.c
- * Author:			P. Bauer
- * Due Date:		October 16, 2012
- * ----------------------------------------------------------
- * Description:
- * Implementation of deletable_primes.h.
- * ----------------------------------------------------------
- */
+#include "deletable_primes.h"
+#include <math.h>
+
+ long remove_digit(int index,  long n){
+  long multi=pow(10,index);
+  return (n/(multi*10))*multi+(n%multi);
+}
+
+int get_ways( long n)
+{
+  if (n<10 && is_prime(n))
+  {
+    return 1;
+  }
+
+  int way=0;
+
+  for (int i = 0; i < get_lenght(n); i++)
+   {
+    if(is_prime(remove_digit(i,n)))
+    {
+      way+=get_ways(remove_digit(i,n));
+    }
+  }
+
+  return way;
+}
+
+bool is_prime(long n)
+{
+  if (n <= 1 || (n % 2 == 0 && n > 2))
+  {
+    return false;
+  }
+
+  for(int i = 3; i <= sqrt(n); i+= 2)
+  {
+    if (n % i == 0)
+      return false;
+  }
+
+  return true;
+}
+
+int get_lenght( long n){
+
+  int lenght=0;
+
+  while (n>1)
+  {
+    n/=10;
+    lenght++;
+  }
+
+  return lenght;
+}
--- deletable_primes.h
+++ deletable_primes.h
@@ -1,16 +1,12 @@
-/*----------------------------------------------------------
- *				HTBLA-Leonding / Klasse: 2AHDV
- * ---------------------------------------------------------
- * Exercise Number: 0
- * Title:			deletable_primes.h
- * Author:			P. Bauer
- * Due Date:		December 1, 2010
- * ----------------------------------------------------------
- * Description:
- * CatCoder Deletable Primes.
- * ----------------------------------------------------------
- */
 #ifndef ___DELETABLE_PRIMES
 #define ___DELETABLE_PRIMES
+
+ long remove_digit(int index, long n);
+
+int get_ways( long n);
+
+bool is_prime( long n);
+
+int get_lenght( long n);
 
 #endif
